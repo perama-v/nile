21de12c27b29d36830803db1e409cd308ac7efdb
perama-v <83961755+perama-v@users.noreply.github.com>
2021-12-23 13:06:48 +1100

    CLI 'forget' to remove *deployments.txt
    Enables a subset of the `nile clean` command.
    
    Workflow loop of (edit-compile-deploy-interact) is hampered by an error with alias name being occupied by the previous loop. 
    
    ```
    Exception: Alias MyContract already exists in localhost.deployments.txt
    ```
    
    This command adds the ability to erase the deployments file for the current network. This feature is provided in `nile clean`, however 
    in a project with many contracts `nile clean` removing the compiled contracts directory is an issue. New loop is: (edit-compile-forget-deploy-interact)

--- src/nile/main.py
+++ src/nile/main.py
@@ -153,6 +153,22 @@ def clean():
 
     print("✨ Workspace clean, keep going!")
 
+@cli.command()
+def forget():
+    """Remove deployment addresses."""
+    local_deployments_filename = f"localhost.{DEPLOYMENTS_FILENAME}"
+    local_accounts_filename = f"localhost.{ACCOUNTS_FILENAME}"
+
+    if os.path.exists(local_deployments_filename):
+        print(f"🚮 Deleting {local_deployments_filename}")
+        os.remove(local_deployments_filename)
+
+    if os.path.exists(local_accounts_filename):
+        print(f"🚮 Deleting {local_accounts_filename}")
+        os.remove(local_accounts_filename)
+
+    print("✨ Old deployments forgotten, ready to deploy!")
+
 
 @cli.command()
 def node():
